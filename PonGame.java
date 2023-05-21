5be001d98ce41e29ed4a466bda9ca2d428e1d152
abhi067 <96076441+abhi067@users.noreply.github.com>
2023-05-21 17:31:32 +0530

    Update PonGame.java

--- PonGame.java
+++ PonGame.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class PonGame {
 
     public static void main(String[] args) {
+        System.out.println("Ping Pong Game by Abhishek Sinha");
         GameFrame frame = new GameFrame();
     }
 }
